179e5029e3640100cb1b0dbf0d592f688ddec6d5
Kishan-Jaiswar <jaiswarkishan78@gmail.com>
2025-03-31 21:03:28 +0530

    First commit

---
+++ backend/mongoConnection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectMongoDb = async () => {
+  try {
+    // await mongoose.connect("mongodb://127.0.0.1:27017/crudNodeExpress");
+    await mongoose.connect("mongodb+srv://jaiswarkishan78:Kishan%401208@mern-crud.cnrxdwn.mongodb.net/crudNodeExpress");
+  } catch (error) {
+    throw new Error("Error", error.message);
+  }
+};
+
+module.exports = connectMongoDb;
